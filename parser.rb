222cf2ecbe0cadede4a504eb1d23dc7ac3ede3c4
Harrison Zandri <email": null>
2016-07-01 14:15:48 -0400

    add filter methods to sorter

--- parser.rb
+++ parser.rb
@@ -1,7 +1,14 @@
 require 'open-uri'
 require 'JSON'
 
-module InspectionSorter
+class InspectionSorter
+
+  attr_reader :violations
+
+  def initialize
+    @violations = get_current_violations
+  end
+
 
   def get_current_violations
     violations = JSON.parse(open("https://data.cityofnewyork.us/resource/9w7m-hzhe.json").read)
@@ -15,18 +22,43 @@ module InspectionSorter
     # end
   end
 
-  def get_violations(data)
-
+  def restaurant_of_the_year
+    restaurant = @violations.min_by do |row|
+      if row["score"].nil?
+        100
+      else
+        row["score"].to_i
+      end
+    end
+    p restaurant
   end
 
-  def by_restaurant(data)
-
+  def get_violations(options)
+    filtered = @violations
+    if !(options['restaurant'].nil?)
+      filtered = by_restaurant(filtered)
+    end
+    if !(options['burrough'].nil?)
+      filtered = by_burrough(filtered)
+    end
+    if !(options['zipcode'].nil?)
+      filtered = by_zip(filtered)
+    end
+    filtered
   end
 
-  def method_name
+  private
 
+  def by_restaurant(restaurant)
+    @violations.select { |row| row["dba"].upcase == restaurant.upcase }
   end
 
+  def by_burrough(burrough)
+    @violations.select { |row| row["boro"].upcase == burrough.upcase }
+  end
 
+  def by_zip(zip)
+    @violations.select { |row| row["zipcode"] == zip }
+  end
 
 end
