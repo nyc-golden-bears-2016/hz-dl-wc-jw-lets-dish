1ac315c7d154400d83105544defd5bc43c16102b
Joe Wood & Wolfgang A Criollo <pair+joe9663.savagesnake@devbootcamp.com>
2016-07-01 14:10:25 -0400

    basic view

---
+++ view.rb
@@ -0,0 +1,9 @@
+class View
+
+    def display_descriptions(restaurant)
+      puts "These are the violations for #{requested}:"
+      puts restaurant["violation_description"]
+      puts "----------------------------------"
+    end
+
+end
